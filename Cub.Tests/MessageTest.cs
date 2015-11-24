0d17f632dace1a17a63b3b46044cf7652a211bff
stebunovd <denis.stebunov@ivelum.com>
2015-11-24 15:40:14 +0300

    add Messages

---
+++ Cub.Tests/MessageTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Cub.Tests
+{
+    class MessageTest
+    {
+        [Test]
+        public void MessageSearch()
+        {
+            // Find messages by FC database
+            var messages = Message.List(new Dictionary<string, object>
+            {
+                { "FCDB", "p1" }
+            });
+            Assert.Greater(messages.Count, 0);
+            var message = messages[0];
+            Assert.GreaterOrEqual(message.TotalOpens, 0);
+            Assert.NotNull(message.Subject);
+            Assert.LessOrEqual(message.Created, DateTime.UtcNow);
+            Assert.AreEqual(message.FCDB, "p1");
+            Assert.Greater(message.FCNewsletterID, 0);
+            // Get particular message by ID
+            var message2 = Message.Get(message.Id);
+            Assert.AreEqual(message.Subject, message2.Subject);
+            Assert.AreEqual(message.FCDB, message2.FCDB);
+            Assert.AreEqual(message.FCNewsletterID, message2.FCNewsletterID);
+            Assert.AreEqual(message.TotalOpens, message2.TotalOpens);
+            Assert.AreEqual(message.Created, message2.Created);
+        }
+    }
+}
---
+++ Cub/Message.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cub
+{
+    public class Message : CObject
+    {
+        public Message() { }
+
+        public Message(Message obj)
+            : base(obj)
+        {
+        }
+
+        public string Subject
+        {
+            get { return _string("subject"); }
+            set { Properties["subject"] = value; }
+        }
+
+        public int TotalOpens
+        {
+            get { return _value<int>("total_opens"); }
+        }
+
+        public string FCDB
+        {
+            get { return _string("fc_db"); }
+            set { Properties["fc_db"] = value; }
+        }
+    
+        public int? FCNewsletterID
+        {
+            get { return _nullableValue<int>("fc_newsletter_id"); }
+            set { Properties["fc_newsletter_id"] = value; }
+        }
+
+        public DateTime? Created
+        {
+            get { return _nullableValue<DateTime>("created"); }
+        }
+        #region Methods
+
+        public static Message Get(string id, string apiKey)
+        {
+            return BaseGet<Message>(id, apiKey);
+        }
+
+        public static Message Get(string id)
+        {
+            return BaseGet<Message>(id, null);
+        }
+
+        public static List<Message> List(Dictionary<string, object> filters, string apiKey)
+        {
+            return BaseList<Message>(filters, apiKey);
+        }
+
+        public static List<Message> List(Dictionary<string, object> filters)
+        {
+            return BaseList<Message>(filters, null);
+        }
+
+        public static List<Message> List(string apiKey)
+        {
+            return BaseList<Message>(null, apiKey);
+        }
+
+        public static List<Message> List()
+        {
+            return BaseList<Message>(null, null);
+        }
+
+
+        #endregion
+    }
+}
