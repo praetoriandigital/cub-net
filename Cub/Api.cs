d26ea8efc50633dec0324e6508c901e066a87532
Oleg Albekov <oleg@albekov.ru>
2018-02-03 05:47:30 +0300

    add methods for uploading files

--- Cub/Api.cs
+++ Cub/Api.cs
@@ -140,5 +140,81 @@ namespace Cub
         {
             return RequestArray(method, url, new Dictionary<string, object>(), apiKey);
         }
+
+        public static string RequestUploadFile(string url, string fileName, Dictionary<string, string> parameters,
+            string contentType = "image/png", string paramName = "file")
+        {
+            var boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
+            var request = (HttpWebRequest) WebRequest.Create(url);
+            request.ContentType = "multipart/form-data; boundary=" + boundary;
+            request.Method = "POST";
+            request.KeepAlive = true;
+            
+            using (var stream = request.GetRequestStream())
+            {
+                var boundaryBytes = Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
+                
+                foreach (var key in parameters.Keys)
+                {
+                    stream.Write(boundaryBytes, 0, boundaryBytes.Length);
+                    var item = $"Content-Disposition: form-data; name=\"{key}\"\r\n\r\n{parameters[key]}";
+                    var data = Encoding.UTF8.GetBytes(item);
+                    stream.Write(data, 0, data.Length);
+                }
+                stream.Write(boundaryBytes, 0, boundaryBytes.Length);
+
+                var name = Path.GetFileName(fileName);
+                var header = "Content-Disposition: form-data; " +
+                             $"name=\"{paramName}\"; " +
+                             $"filename=\"{name}\"\r\n" +
+                             $"Content-Type: {contentType}\r\n\r\n";
+                var headerBytes = Encoding.UTF8.GetBytes(header);
+                stream.Write(headerBytes, 0, headerBytes.Length);
+
+                using (var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read))
+                {
+                    var buffer = new byte[4096];
+                    int bytesRead;
+                    while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
+                    {
+                        stream.Write(buffer, 0, bytesRead);
+                    }
+                }
+
+                var trailer = Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
+                stream.Write(trailer, 0, trailer.Length);
+            }
+
+            using (var response = request.GetResponse())
+            {
+                return ReadStream(response.GetResponseStream());
+            }
+        }
+
+        public static void UploadImage(string filename, string url, string apiKey)
+        {
+            // Get credentials for uploading to S3
+            var parameters = new Dictionary<string, object>
+            {
+                ["filename"] = Path.GetFileName(filename)
+            };
+            var res = RequestObject("GET", url, parameters, apiKey);
+            var upploadUrl = (string) res["action"];
+            var fields = res["fields"];
+            var formData = new Dictionary<string, string>();
+            foreach (var field in fields)
+            {
+                formData[field["name"].Value<string>()] = field["value"].Value<string>();
+            }
+
+            // Upload to S3
+            RequestUploadFile(upploadUrl, filename, formData);
+            RequestObject("POST", url, parameters, apiKey);
+        }
+
+        public static void DeleteImage(string url, string apiKey)
+        {
+            RequestObject("DELETE", url, apiKey);
+        }
     }
 }
